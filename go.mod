2929ee6b649fdc48eebfe112d9295e2be7642ef5
vector-10 <duzieblaise10@gmail.com>
2026-05-23 14:52:38 +0100

    feat: added support for url storage on Redis

--- go.mod
+++ go.mod
@@ -3,6 +3,12 @@ module github.com/vector-10/url-shortner
 go 1.26.2
 
 require (
-	github.com/google/uuid v1.6.0 // indirect
-	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e // indirect
+	github.com/google/uuid v1.6.0
+	github.com/redis/go-redis/v9 v9.19.0
+	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
+)
+
+require (
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
 )
---
+++ internal/store/redis.go
@@ -0,0 +1,65 @@
+package store
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"time"
+	"github.com/redis/go-redis/v9"
+	"github.com/vector-10/url-shortner/internal/models"
+)
+
+
+type RedisStore struct {
+	client *redis.Client
+
+}
+
+
+func NewRedisStore(addr string) *RedisStore {
+	client := redis.NewClient(&redis.Options{
+		Addr: addr,
+	})
+	return &RedisStore{client: client}
+}
+
+func (r *RedisStore) Save(record *models.URLRecord) error {
+	data, err := json.Marshal(record)
+
+	if err != nil {
+		return err
+	}
+
+	ttl := time.Until(record.ExpiresAt)
+	ctx := context.Background()
+
+	return r.client.Set(ctx, record.Slug, data, ttl).Err()
+}
+
+func (r *RedisStore) GetBySlug(slug string) (*models.URLRecord, error) {
+	ctx := context.Background()
+	data, err := r.client.Get(ctx, slug).Bytes()
+	if err == redis.Nil {
+		return nil, errors.New("Key not found")
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	var record models.URLRecord
+	if err := json.Unmarshal(data, &record); err != nil {
+		return nil, err
+	}
+
+	return &record, nil
+}
+
+func (r *RedisStore) IncrementClicks(slug string) error {
+	record, err := r.GetBySlug(slug)
+	if err != nil {
+		return err
+	}
+
+	record.Clicks++
+	return r.Save(record)
+}
